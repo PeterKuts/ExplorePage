f038aa52e41af3abbd1a8ffaeb7958889a46e1fc
Peter Kuts <peter.o.kuts@gmail.com>
2016-05-16 13:50:09 +0300

    initial commit

---
+++ ExplorePage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExplorePage
+//
+//  Created by user on 5/16/16.
+//  Copyright © 2016 peterkuts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
